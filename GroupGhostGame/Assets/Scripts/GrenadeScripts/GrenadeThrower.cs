916a0fc19e5789b3cc47accbb5c98bbb281bd0bd
PythonBlackBelt <pythonblackbelt@gmail.com>
2024-11-09 13:26:13 -0500

    Polished Grenade Spawning

--- GroupGhostGame/Assets/Scripts/GrenadeScripts/GrenadeThrower.cs
+++ GroupGhostGame/Assets/Scripts/GrenadeScripts/GrenadeThrower.cs
@@ -1,25 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class GrenadeThrower : MonoBehaviour
 {
 
     public float throwForce = 40f;
     public GameObject grenadePrefab;
+    public float coolDown = 1.5f;
+    private float counter = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.G))
+        if (Input.GetKey(KeyCode.G) && counter >= coolDown)
         {
             ThrowGrenade();
+            counter = 0f;
         }
+
+        counter += Time.deltaTime;
     }
 
     void ThrowGrenade()
     {
-       GameObject grenade = Instantiate(grenadePrefab,transform.position, transform.rotation);
+        Vector3 spawnPos = transform.position + (transform.forward * 10);
+        GameObject grenade = Instantiate(grenadePrefab, spawnPos, transform.rotation);
         Rigidbody rb = grenade.GetComponent<Rigidbody>();
         rb.AddForce(transform.forward  * throwForce, ForceMode.VelocityChange);
     }
